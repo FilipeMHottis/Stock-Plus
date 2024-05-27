136dbfc4f1a60df0fd4e80e50e9a4f2ebd4ad0e5
Filipe Marques Hottis <lipehottis@gmail.com>
2024-05-27 13:01:12 -0300

    feat: create page login

--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 
 def login(request):
-    return render(request, "login.html")
+    messages = []
+
+    if request.method == "GET":
+        return render(request, "login.html")
+
+    elif request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        try:
+            user = User.objects.get(username=username)
+
+            if user.check_password(password):
+                messages.append("Login successful")
+            else:
+                messages.append("Incorrect password")
+
+        except User.DoesNotExist:
+            messages.append("User does not exist")
+
+        return render(request, "login.html", {"messages": messages})
